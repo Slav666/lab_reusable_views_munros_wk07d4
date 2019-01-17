df6706408616d681b906a6c782cfaf4ba8607fc3
Jonathan Lavi <jonathan.lavi@gmail.com>
2019-01-17 13:19:31 +0000

    set up file structure

---
+++ src/app.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', () => {
+  console.log('JavaScript Loaded');
+
+  // initiate munro_list_view, bindEvents
+
+  // initiate munro object, bindEvents
+});
---
+++ src/views/munro_list_view.js
@@ -0,0 +1,5 @@
+// subscribe to munro data
+
+// receive munro data (array of munro information objects)
+
+// for each munro in the array: ask munro_details.js to create a view
